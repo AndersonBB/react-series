4f212c2a6f55f80bebb6cea8090c7d6e5ad11c82
Anderson B <anderson.bb32@hotmail.com>
2018-06-22 17:13:43 -0300

    Iniciando Projeto

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // É utilizado sempre que for renderizar os componentes (render)
+import App from './components/App'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app") 
+);
